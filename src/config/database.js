60f4c4fbc1d5eeab96c0ca70f1181e16dcce9f2a
faisalalm01 <faisalali2858@gmail.com>
2021-05-31 20:07:17 +0700

    edit

--- src/config/database.js
+++ src/config/database.js
@@ -6,20 +6,20 @@ module.exports = {
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
+  // "test": {
+  //   "username": "root",
+  //   "password": null,
+  //   "database": "database_test",
+  //   "host": "127.0.0.1",
+  //   "dialect": "mysql"
+  // },
+  // "production": {
+  //   "username": "root",
+  //   "password": null,
+  //   "database": "database_production",
+  //   "host": "127.0.0.1",
+  //   "dialect": "mysql"
+  // },
   "production": {
     use_env_variable: 'DATABASE_URL',
     dialect: "postgress",
--- src/routes/booksRoutes.js
+++ src/routes/booksRoutes.js
@@ -6,9 +6,15 @@ const authMiddleware = require("../helpers/authMiddleware");
 booksRoutes.get("/",
 //  authMiddleware.checkLogin, 
 booksControllers.getAllBooks);
-booksRoutes.post("/",authMiddleware.checkLogin,booksControllers.postBooks);
+booksRoutes.post("/",
+// authMiddleware.checkLogin,
+booksControllers.postBooks);
 booksRoutes.get("/:id",booksControllers.getDataById);
-booksRoutes.delete("/:id",authMiddleware.checkLogin, booksControllers.deleteDataById);
-booksRoutes.put("/:id",authMiddleware.checkLogin, booksControllers.putDataById);
+booksRoutes.delete("/:id",
+// authMiddleware.checkLogin,
+booksControllers.deleteDataById);
+booksRoutes.put("/:id",
+// authMiddleware.checkLogin, 
+booksControllers.putDataById);
 
 module.exports = booksRoutes;
